0a4296f6f78f390a980eb4a5aa0cdbee70fa4909
Kasia Gasperowicz <56889953+kasiagasperowicz@users.noreply.github.com>
2026-02-24 19:19:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php get_header(); ?>
 
 <main>
-    <!-- <h1>To jest Sunnycode</h1> -->
+   
 
-    <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) :
-            the_post();
-            the_content();
-        endwhile;
-    endif;
-    ?>
+    
 </main>
 
 <?php get_footer(); ?>
